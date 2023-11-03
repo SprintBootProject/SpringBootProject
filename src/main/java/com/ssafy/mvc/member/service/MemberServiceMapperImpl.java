e98664aaf0df105ac2ea7e29743e6da4bdad44f8
yujin1698 <yujin1698@naver.com>
2023-11-03 12:03:01 +0900

    멤버 관련 패키지, 파일 추가

--- src/main/java/com/ssafy/mvc/member/service/MemberServiceMapperImpl.java
+++ src/main/java/com/ssafy/mvc/member/service/MemberServiceMapperImpl.java
@@ -1,5 +1,48 @@
 package com.ssafy.mvc.member.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ssafy.mvc.member.dto.MemberDto;
+import com.ssafy.mvc.member.repository.MemberRepository;
+
 public class MemberServiceMapperImpl implements MemberService {
+
+	private MemberRepository repo;
+	
+	@Autowired
+	public MemberServiceMapperImpl(MemberRepository repo) {
+		super();
+		this.repo = repo;
+	}
+
+	@Override
+	public void register(String id, String name, String pass) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MemberDto update(String id, String name, String pass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MemberDto login(String id, String pass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findPw(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
