b9d6559ece70946f1c9790e568422a99463304a5
yujin1698 <yujin1698@naver.com>
2023-11-03 15:10:49 +0900

    member rest 방식 api 구현

--- src/main/java/com/ssafy/mvc/member/service/MemberService.java
+++ src/main/java/com/ssafy/mvc/member/service/MemberService.java
@@ -1,19 +1,21 @@
 package com.ssafy.mvc.member.service;
 
+import java.util.List;
+
 import com.ssafy.mvc.member.dto.MemberDto;
 
 public interface MemberService {
 	// 회원가입
-	public void register(String id, String name, String pass);
+	public void register(MemberDto dto) throws Exception;
 	// 회원수정
-	public MemberDto update(String id, String name, String pass);
-	// 회원조회
-	//public MemberDto research(String id);
+	public void update(MemberDto dto) throws Exception;
+	// 모든 회원조회
+	public List<MemberDto> listMember() throws Exception;
 	// 회원탈퇴
-	public void delete(String id);
+	public void delete(String id) throws Exception;
 	// 로그인
-	public MemberDto login(String id, String pass);
+	public MemberDto login(MemberDto dto) throws Exception;
 	// 로그아웃 -> 컨트롤러에서 구현 
 	// 비밀번호 찾기 
-	public String findPw(String id);
+	public String findPw(String id) throws Exception;
 }
--- src/main/java/com/ssafy/mvc/member/service/MemberServiceMapperImpl.java
+++ src/main/java/com/ssafy/mvc/member/service/MemberServiceMapperImpl.java
@@ -1,48 +1,57 @@
 package com.ssafy.mvc.member.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ssafy.mvc.member.dto.MemberDto;
 import com.ssafy.mvc.member.repository.MemberRepository;
 
+@Service("MemberServiceMapperImpl")
 public class MemberServiceMapperImpl implements MemberService {
 
 	private MemberRepository repo;
 	
 	@Autowired
-	public MemberServiceMapperImpl(MemberRepository repo) {
+	public MemberServiceMapperImpl(MemberRepository repo) throws Exception{
 		super();
 		this.repo = repo;
 	}
 
 	@Override
-	public void register(String id, String name, String pass) {
+	public void register(MemberDto dto) throws Exception{
+		repo.register(dto);
+	}
+
+	@Override
+	public void update(MemberDto dto) throws Exception{
 		// TODO Auto-generated method stub
+		repo.update(dto);
+	}
+
+	@Override
+	public void delete(String id) throws Exception{
+		repo.delete(id);
 		
 	}
 
 	@Override
-	public MemberDto update(String id, String name, String pass) {
+	public MemberDto login(MemberDto dto) throws Exception{
 		// TODO Auto-generated method stub
-		return null;
+		return repo.login(dto);
 	}
 
 	@Override
-	public void delete(String id) {
+	public String findPw(String id) throws Exception{
 		// TODO Auto-generated method stub
-		
+		return repo.findPw(id);
 	}
 
 	@Override
-	public MemberDto login(String id, String pass) {
+	public List<MemberDto> listMember() throws Exception{
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String findPw(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return repo.listMember();
 	}
 
 }
